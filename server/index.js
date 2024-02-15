d686883942174e36e846b9b1d575eff547c8ef8b
camillegonzales <gonzcami@oregonstate.edu>
2024-02-15 01:49:43 -0800

    set up GET endpoint route to test

--- server/index.js
+++ server/index.js
@@ -4,5 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
+app.use('/', require('./routes/authRoutes'));
+
 const PORT = 8000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
