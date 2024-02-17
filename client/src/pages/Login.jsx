d5a9d4631c3ed02cbbb14cb488c6187c46c9c64b
camillegonzales <gonzcami@oregonstate.edu>
2024-02-17 15:15:55 -0800

    set up JSONWeb token

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -17,6 +17,8 @@ export default function Login() {
       const {data} = await axios.post('/login', {
         email,
         password,
+      }, {
+        withCredentials:true
       });
       if (data.error) {
         toast.error(data.error);
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const { hashPassword, comparePassword } = require('../helpers/auth');
+const jwt = require('jsonwebtoken');
 
 const test = (req, res) => {
     res.json('test is working')
@@ -58,7 +59,10 @@ const loginUser = async (req, res) => {
         // Check if passwords match
         const match = await comparePassword(password, user.password);
         if (match) {
-            res.json('Passwords match');
+            jwt.sign({email: user.email, id: user._id, name: user.name}, process.env.JWT_SECRET, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json(user)
+            });
         } else {
             return res.json({
                 error: 'Incorrect password'
