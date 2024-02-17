f8572976a1646861aaa27a90cb505e1a43670a61
camillegonzales <gonzcami@oregonstate.edu>
2024-02-16 20:21:27 -0800

    implemented bcrypt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const { hashPassword, comparePassword } = require('../helpers/auth');
 
 const test = (req, res) => {
     res.json('test is working')
@@ -11,24 +12,28 @@ const registerUser = async (req, res) => {
         if (!name) {
             return res.json({
                 error: 'Name is required'
-            })
+            });
         }
         // Check if password is valid
         if (!password || password.length < 6) {
             return res.json({
                 error: 'Password is required and should be at least 6 characters long'
-            })
+            });
         }
         // Check email
         const exist = await User.findOne({email});
         if (exist) {
             return res.json({
                 error: 'Email is already taken'
-            })
+            });
         }
 
+        const hashedPassword = await hashPassword(password);
+        // Create user in database
         const user = await User.create({
-            name, email, password
+            name, 
+            email, 
+            password: hashedPassword,
         });
 
         return res.json(user);
